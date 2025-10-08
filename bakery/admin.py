c226465636588fbe5bf424528a5f2847f698bf5c
Priyanshu Raj <priyanshuraj121522@gmail.com>
2025-10-08 09:01:16 +0000

    admin: add search_fields to related ModelAdmins referenced by autocomplete_fields (fix admin.E040)

--- bakery/admin.py
+++ bakery/admin.py
@@ -20,11 +20,51 @@ class UserProfileAdmin(admin.ModelAdmin):
     search_fields = ("user__username", "user__email", "outlet__name")  # makes this list searchable too
     autocomplete_fields = ["user", "outlet"]                           # now valid because Outlet has search_fields
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
 # ---- Register the rest normally ----
 rest_models = [
-    Ingredient, Product, Recipe, RecipeItem,
+    Ingredient, Recipe, RecipeItem,
     Batch, Dispatch, DispatchLine, Sale, SaleItem,
     Wastage, StockLedger
 ]
 for m in rest_models:
     admin.site.register(m)
+
+# COGS START
+class RecipeItemInline(admin.TabularInline):
+    model = RecipeItem
+    fields = ("ingredient", "qty_per_unit", "wastage_pct")
+    extra = 1
+
+
+class IngredientCostingAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "uom", "unit_cost", "active")
+    list_filter = ("active",)
+    search_fields = ("name",)
+
+
+class RecipeCostingAdmin(admin.ModelAdmin):
+    list_display = ("id", "product")
+    autocomplete_fields = ("product",)
+    inlines = [RecipeItemInline]
+    search_fields = ("product__name",)
+    search_fields = ("product__name",)
+
+
+class RecipeItemCostingAdmin(admin.ModelAdmin):
+    list_display = ("id", "recipe", "ingredient", "qty_per_unit", "wastage_pct")
+    autocomplete_fields = ("recipe", "ingredient")
+    list_select_related = ("recipe", "ingredient")
+    search_fields = ("recipe__product__name", "ingredient__name")
+
+
+admin.site.unregister(Ingredient)
+admin.site.unregister(Recipe)
+admin.site.unregister(RecipeItem)
+admin.site.register(Ingredient, IngredientCostingAdmin)
+admin.site.register(Recipe, RecipeCostingAdmin)
+admin.site.register(RecipeItem, RecipeItemCostingAdmin)
+# COGS END
